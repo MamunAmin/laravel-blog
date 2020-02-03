2e144ea6cb6e41d0c8fdc3ec75712662b2726986
NumamsA <mamun.amin1994@gmail.com>
2020-02-03 21:57:25 +0600

    new code

--- resources/views/post/creat.blade.php
+++ resources/views/post/creat.blade.php
@@ -1,15 +1,14 @@
 @extends('extendFrom')
 
 @section('header')
-<header class="masthead" style="background-image: url({{ asset('blog/img/home-bg.jpg')}})">
+<header class="masthead">
   <div class="overlay"></div>
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="site-heading">
-          <h1>Feel Your Earth</h1>
-          <span class="subheading">For Discovering Every Corner of This Earth!!</span>
-        </div>
+          <div class="page-header">
+            <h1 style="height: 55px"></h1>
+          </div>
       </div>
     </div>
   </div>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'HomeController@index');
+
+/** Route for posts CRUD**/
+Route::get('post/creat/', 'PostController@index');

43a16f18e147818cd4d0e95d251d6e8b230125f9
NumamsA <mamun.amin1994@gmail.com>
2020-02-11 18:29:12 +0600

    add

--- resources/views/post/creat.blade.php
+++ resources/views/post/creat.blade.php
@@ -16,40 +16,66 @@
 @endsection
 
 @section('content')
-<div class="row">
-  <div class="col-lg-8 col-md-10 mx-auto">
-    <div class="post-preview">
-      <a href="post.html">
-        <h2 class="post-title">
-          Man must explore, and this is exploration at its greatest
-        </h2>
-        <h3 class="post-subtitle">
-          Problems look mighty small from 150 miles up
-        </h3>
-      </a>
-      <p class="post-meta">Posted by
-        <a href="#">Start Bootstrap</a>
-        on September 24, 2019</p>
-    </div>
-    <hr>
-    <div class="post-preview">
-      <a href="post.html">
-        <h2 class="post-title">
-          Failure is not an option
-        </h2>
-        <h3 class="post-subtitle">
-          Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-        </h3>
-      </a>
-      <p class="post-meta">Posted by
-        <a href="#">Start Bootstrap</a>
-        on July 8, 2019</p>
-    </div>
-    <hr>
-    <!-- Pager -->
-    <div class="clearfix">
-      <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-    </div>
-  </div>
+<div class="container">
+   <div class="row">
+     <div class="col-lg-8 col-md-10 mx-auto">
+       
+        <a href="{{ route('add.category') }}" class="btn btn-danger">Add Category</a>
+        <a href="{{ route('all.category') }}" class="btn btn-info">All Category</a>
+        <a href="{{ route('all.post') }}" class="btn btn-info">All Post</a>
+       
+      <hr>
+      @if ($errors->any())
+          <div class="alert alert-danger">
+              <ul>
+                  @foreach ($errors->all() as $error)
+                      <li>{{ $error }}</li>
+                  @endforeach
+              </ul>
+          </div>
+       @endif
+
+       <form action="{{ route('store.post') }}" method="post" enctype="multipart/form-data">
+        @csrf
+         <div class="control-group">
+           <div class="form-group floating-label-form-group controls">
+             <label>Post Title</label>
+             <input type="text" class="form-control" placeholder="Title" name="title" required >
+           </div>
+         </div>
+         <br>
+          <div class="control-group">
+           <div class="form-group floating-label-form-group controls">
+             <label>Category</label>
+            <select class="form-control" name="category_id">
+              @foreach($category as $row)
+              <option value="{{ $row->id }}">{{ $row->name }}</option>
+              @endforeach
+            </select>
+           </div>
+         </div>
+         <br>
+         <div class="control-group">
+           <div class="form-group col-xs-12 floating-label-form-group controls">
+             <label>Post Image</label>
+             <input type="file" class="form-control"  required name="image">
+             
+           </div>
+         </div>
+         <div class="control-group">
+           <div class="form-group floating-label-form-group controls">
+             <label>Post Details</label>
+             <textarea rows="5" class="form-control" placeholder="Details"  required name="details"></textarea>
+            
+           </div>
+         </div>
+         <br>
+         <div id="success"></div>
+         <div class="form-group">
+           <button type="submit" class="btn btn-primary" id="sendMessageButton">Send</button>
+         </div>
+       </form>
+     </div>
+   </div>
 </div>
 @endsection
